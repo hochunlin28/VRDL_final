a92a4d6d3516dc67bf6935686e9480b9881cb1f0
Eri <36273909+eritup45@users.noreply.github.com>
2022-01-10 16:59:12 +0800

    autopep8

--- ensemble.py
+++ ensemble.py
@@ -1,7 +1,7 @@
 """ 
 Ensembling methods for object detection.
-    # USAGE: edit a, b as the csv file you want to ensemble
-    # If you want to change size of width, height, modify "convert_ratio". 
+    # USAGE: edit a, b, c as the csv file you want to ensemble
+    # If you want to change size of width, height, modify convert_ratio. 
     # There should be './stage_2_sample_submission.csv' in your working directory.
     # Output will be './ensemble_answer.csv'
 """
@@ -33,9 +33,11 @@ Input:
 Output:
     A list of boxes, on the same format as the input. Confidences are in range 0-1.
 """
-def GeneralEnsemble(dets, iou_thresh = 0.5, weights=None):
+
+
+def GeneralEnsemble(dets, iou_thresh=0.5, weights=None):
     assert(type(iou_thresh) == float)
-    
+
     ndets = len(dets)
     # print(f'ndets: {ndets}')
 
@@ -63,29 +65,29 @@ def GeneralEnsemble(dets, iou_thresh = 0.5, weights=None):
         weights = [w]*(ndets + empty_pred_cnt)
     else:
         assert(len(weights) == ndets)
-        
+
         s = sum(weights)
         for i in range(0, len(weights)):
             weights[i] /= s
 
     out = list()
     used = list()
-    
-    for idet in range(0,ndets):
+
+    for idet in range(0, ndets):
         det = dets[idet]
         for box in det:
             if box in used:
                 continue
-                
+
             used.append(box)
             # Search the other detectors for overlapping box of same class
             found = []
             for iodet in range(0, ndets):
                 odet = dets[iodet]
-                
+
                 if odet == det:
                     continue
-                
+
                 bestbox = None
                 bestiou = iou_thresh
                 for obox in odet:
@@ -97,12 +99,12 @@ def GeneralEnsemble(dets, iou_thresh = 0.5, weights=None):
                             if iou > bestiou:
                                 bestiou = iou
                                 bestbox = obox
-                                
+
                 if not bestbox is None:
                     w = weights[iodet]
-                    found.append((bestbox,w))
+                    found.append((bestbox, w))
                     used.append(bestbox)
-                            
+
             # Now we've gone through all other detectors
             if len(found) == 0:
                 new_box = list(box)
@@ -121,13 +123,13 @@ def GeneralEnsemble(dets, iou_thresh = 0.5, weights=None):
             else:
                 allboxes = [(box, weights[idet])]
                 allboxes.extend(found)
-                
+
                 xc = 0.0
                 yc = 0.0
                 bw = 0.0
                 bh = 0.0
                 conf = 0.0
-                
+
                 wsum = 0.0
                 for bb in allboxes:
                     w = bb[1]
@@ -139,11 +141,11 @@ def GeneralEnsemble(dets, iou_thresh = 0.5, weights=None):
                     bw += w*b[2]
                     bh += w*b[3]
                     conf += w*b[5]
-                
+
                 xc /= wsum
                 yc /= wsum
                 bw /= wsum
-                bh /= wsum    
+                bh /= wsum
 
                 # new_box = [xc, yc, bw, bh, box[4], conf]
 
@@ -153,42 +155,46 @@ def GeneralEnsemble(dets, iou_thresh = 0.5, weights=None):
 
                 out.append(new_box)
     return out
-    
+
+
 def getCoords(box):
     x1 = float(box[0]) - float(box[2])/2
     x2 = float(box[0]) + float(box[2])/2
     y1 = float(box[1]) - float(box[3])/2
     y2 = float(box[1]) + float(box[3])/2
     return x1, x2, y1, y2
-    
+
+
 def computeIOU(box1, box2):
     x11, x12, y11, y12 = getCoords(box1)
     x21, x22, y21, y22 = getCoords(box2)
-    
-    x_left   = max(x11, x21)
-    y_top    = max(y11, y21)
-    x_right  = min(x12, x22)
+
+    x_left = max(x11, x21)
+    y_top = max(y11, y21)
+    x_right = min(x12, x22)
     y_bottom = min(y12, y22)
 
     if x_right < x_left or y_bottom < y_top:
-        return 0.0    
-        
+        return 0.0
+
     intersect_area = (x_right - x_left) * (y_bottom - y_top)
     box1_area = (x12 - x11) * (y12 - y11)
-    box2_area = (x22 - x21) * (y22 - y21)        
-    
+    box2_area = (x22 - x21) * (y22 - y21)
+
     iou = intersect_area / (box1_area + box2_area - intersect_area)
     return iou
 
 # return: [[box_x, box_y, box_w, box_h, class, confidence] * n1]
 # bbox_str: [(conf, x1, y1, width, height) * n2]
-def str2list(bbox_str, convert_ratio = 1):
+
+
+def str2list(bbox_str, convert_ratio=1):
     if bbox_str == '':  # empty str
         return [[]]
     res = []
     cnt = 0
     bbox_list = bbox_str.split(' ')
-    
+
     while(cnt < len(bbox_list)):
         confidence = float(bbox_list[0 + cnt])
         x = float(bbox_list[1 + cnt])
@@ -202,7 +208,6 @@ def str2list(bbox_str, convert_ratio = 1):
         w = float(bbox_list[3 + cnt]) * convert_ratio
         h = float(bbox_list[4 + cnt]) * convert_ratio
 
-
         temp = [xc, yc, w, h, 0, confidence]
         res.append(temp)
         cnt += 5
@@ -212,6 +217,7 @@ def str2list(bbox_str, convert_ratio = 1):
     # for i in range(len(bbox_list)):
     #     if i % 5 == 0:
 
+
 def csv2list(csv_path):
     with open(csv_path, 'r') as f:
         file = csv.reader(f)
@@ -219,7 +225,9 @@ def csv2list(csv_path):
     return my_list
 
 # Get the bboxes from one same id
-def combine_by_id(list_of_csv, id,  weights = None):
+
+
+def combine_by_id(list_of_csv, id,  weights=None):
     total_csv = len(list_of_csv)
     csv_bboxes = []
     for i in range(len(list_of_csv)):
@@ -228,21 +236,22 @@ def combine_by_id(list_of_csv, id,  weights = None):
                 id_bboxes = str2list(csv[1], convert_ratio=convert_ratio)
                 # print(id_bboxes)
                 csv_bboxes.append(id_bboxes)
-    
+
     # print(csv_bboxes)
-    ens = GeneralEnsemble(csv_bboxes, weights = weights)
+    ens = GeneralEnsemble(csv_bboxes, weights=weights)
     print(ens)
     return ens
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     # Toy example
-    # dets = [ 
+    # dets = [
     #         [[0.1, 0.1, 1.0, 1.0, 0, 0.9], [1.2, 1.4, 0.5, 1.5, 0, 0.9]],
     #         [[0.2, 0.1, 0.9, 1.1, 0, 0.8]],
     #         [[5.0,5.0,1.0,1.0,0,0.5]],
     #         [[]]
     #        ]
-    # dets = [ 
+    # dets = [
     #         [[]],
     #         [[]],
     #         [[]],
@@ -269,7 +278,7 @@ if __name__=="__main__":
     # id = '00271e8e-aea8-4f0a-8a34-3025831f1079'
     # id_ans = combine_by_id(list_of_csv, id)
 
-    df = pd.read_csv('stage_2_sample_submission.csv') 
+    df = pd.read_csv('stage_2_sample_submission.csv')
     for id in df['patientId']:
         str_id_ans = ''
         temp_list = []
@@ -279,7 +288,6 @@ if __name__=="__main__":
             for s in ans:
                 temp_list.append(s)
         temp_str = ' '.join(str(i) for i in temp_list)
-        df['PredictionString'].loc[df['patientId']==id] = temp_str
+        df['PredictionString'].loc[df['patientId'] == id] = temp_str
 
     df.to_csv('./ensemble_answer.csv', encoding='utf-8', index=False)
-
